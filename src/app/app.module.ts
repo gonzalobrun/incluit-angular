8c41b9c968b778a5245a1f9f91c50093fb46302a
Gonzalo <gonzalobrun11@gmail.com>
2019-08-04 02:06:34 -0300

    Se agrego libreria ngx-bootstrap

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,6 +7,7 @@ import { SharedModule } from './shared/shared.module';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './pages/home/home.component';
 import { MyTasksComponent } from './pages/my-tasks/my-tasks.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
   declarations: [
@@ -17,7 +18,8 @@ import { MyTasksComponent } from './pages/my-tasks/my-tasks.component';
   imports: [
     BrowserModule,
     AppRoutingModule,
-    SharedModule
+    SharedModule,
+    BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
