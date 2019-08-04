29ad32d7dbfdd214fc5840d49b9316384974ffe4
Gonzalo <gonzalobrun11@gmail.com>
2019-08-04 01:51:05 -0300

    Se agrego SharedModule, y HeaderComponent con estilos

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,6 +2,8 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
+import { SharedModule } from './shared/shared.module';
+
 import { AppComponent } from './app.component';
 import { HomeComponent } from './pages/home/home.component';
 import { MyTasksComponent } from './pages/my-tasks/my-tasks.component';
@@ -14,7 +16,8 @@ import { MyTasksComponent } from './pages/my-tasks/my-tasks.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    SharedModule
   ],
   providers: [],
   bootstrap: [AppComponent]
