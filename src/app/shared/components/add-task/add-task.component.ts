2fcf12a33a58421e61cb46747fd18b88291f374a
Gonzalo <gonzalobrun11@gmail.com>
2019-08-05 00:34:36 -0300

    Se creo formulario para crear tasks

--- src/app/shared/components/add-task/add-task.component.ts
+++ src/app/shared/components/add-task/add-task.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit, TemplateRef } from '@angular/core';
 import { BsModalService, BsModalRef } from 'ngx-bootstrap/modal';
+import { FormGroup, FormControl } from '@angular/forms';
 
 @Component({
   selector: 'app-add-task',
@@ -9,10 +10,25 @@ import { BsModalService, BsModalRef } from 'ngx-bootstrap/modal';
 export class AddTaskComponent implements OnInit {
 
   public modalRef: BsModalRef;
+  public newTaskForm: FormGroup;
 
   constructor(private modalService: BsModalService) {}
 
   ngOnInit() {
+    this.buildForm();
+  }
+
+  buildForm(): void {
+    this.newTaskForm = new FormGroup({
+      title: new FormControl(''),
+      description: new FormControl(''),
+      date: new FormControl(new Date()),
+      deadline: new FormControl('')
+    })
+  }
+
+  confirmTask(): void {
+    console.log(this.newTaskForm.value);
   }
 
   public openAddModal(template: TemplateRef<any>): void {
--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -2,6 +2,7 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ReactiveFormsModule } from '@angular/forms';
 import { ModalModule } from 'ngx-bootstrap/modal';
+import { BsDatepickerModule } from 'ngx-bootstrap/datepicker';
 
 import { HeaderComponent } from './components/header/header.component';
 import { TasksListComponent } from './components/tasks-list/tasks-list.component';
@@ -18,7 +19,8 @@ import { AddTaskComponent } from './components/add-task/add-task.component';
   imports: [
     CommonModule,
     ReactiveFormsModule,
-    ModalModule.forRoot()
+    ModalModule.forRoot(),
+    BsDatepickerModule.forRoot(),
   ],
   exports: [
     HeaderComponent,
